e87579a7d4d0c24ec9084bf324f01d1d51d08b70
versus33 <versus333@gmail.com>
2022-01-18 00:30:03 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const { createWindow } = require("./main");
-const { app } = require("electron");
-
-require('./database');
-
-require('electron-reload')(__dirname);
-
-app.allowRendererProcessReuse = true;
-app.whenReady().then(createWindow);
